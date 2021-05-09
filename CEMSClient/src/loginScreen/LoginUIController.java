8a2fadea49fe06c596f9670e07d72cfbd5c04dfe
TalAmar <f914gt29ra12@gmail.com>
2021-05-09 20:03:26 +0300

    TeacherDashboard

--- CEMSClient/src/loginScreen/LoginUIController.java
+++ CEMSClient/src/loginScreen/LoginUIController.java
@@ -6,9 +6,6 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 
-import javafx.animation.TranslateTransition;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -16,8 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.util.Duration;
-import util.Navigator;
+import util.*;
 
 public class LoginUIController{
 
@@ -40,6 +36,7 @@ public class LoginUIController{
 	private AnchorPane anchorLogin;
 
 	private Node teacherDashboard;
+	private static final int menuMovementRightToLeft = -1280 + 283 - 1;
 
 	/**
 	 * clicking login with a correct user name and password will open the relevant
@@ -54,7 +51,7 @@ public class LoginUIController{
 		 * moveItem's lambda expression will change according to the user's permissions
 		 * using switch case: 1- teacher, 2- student, 3- principle
 		 * ------------------------------------------------------------------------------------------------*/
-		moveItem(menuVBox, -900 + 283 - 1, 1, (e) -> {
+		GeneralUIMethods.moveItem(menuVBox, menuMovementRightToLeft, 1, (e) -> {
 			try {
 				teacherDashboard = FXMLLoader.load(getClass().getResource(Navigator.TEACHERDASHBOARD.getVal()));
 				anchorLogin.getChildren().setAll(teacherDashboard);
@@ -63,32 +60,18 @@ public class LoginUIController{
 			}
 		});
 
-		moveItem(usernameTxt, 0, 0.55, (e) -> {
+		GeneralUIMethods.moveItem(usernameTxt, 0, 0.55, (e) -> {
 			usernameTxt.setVisible(false);
 		});
-		moveItem(passwordTxt, 0, 0.55, (e) -> {
+		GeneralUIMethods.moveItem(passwordTxt, 0, 0.55, (e) -> {
 			passwordTxt.setVisible(false);
 		});
-		moveItem(welcomeLbl, 0, 0.55, (e) -> {
+		GeneralUIMethods.moveItem(welcomeLbl, 0, 0.55, (e) -> {
 			welcomeLbl.setVisible(false);
 		});
-		moveItem(loginBtn, 0, 0.55, (e) -> {
+		GeneralUIMethods.moveItem(loginBtn, 0, 0.55, (e) -> {
 			loginBtn.setVisible(false);
 		});
-	}
-
-	/**
-	 * moves element on the screen "layoutX" pixels in "time" seconds.
-	 * 
-	 * @param o
-	 * @param layoutX
-	 * @param time
-	 */
-	private void moveItem(Object o, double layoutX, double time, EventHandler<ActionEvent> e) {
-		TranslateTransition t = new TranslateTransition(Duration.seconds(time), (Node) o);
-		t.setToX(layoutX);
-		t.play();
-		t.setOnFinished(e);
 	}
 
 }
--- CEMSClient/src/teacherDashboard/teacherDashboardUIController.java
+++ CEMSClient/src/teacherDashboard/teacherDashboardUIController.java
@@ -1,5 +1,6 @@
 package teacherDashboard;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 
@@ -8,4 +9,26 @@ public class teacherDashboardUIController {
     @FXML
     private VBox menuVBox;
 
+    @FXML
+    private JFXButton setDateForTests;
+
+    @FXML
+    private JFXButton viewActiveTests;
+
+    @FXML
+    private JFXButton testBank;
+
+    @FXML
+    private JFXButton questionBank;
+
+    @FXML
+    private JFXButton testReports;
+
+    @FXML
+    private JFXButton scheduledTests;
+
+    @FXML
+    private JFXButton signOut;
+    
+    
 }
