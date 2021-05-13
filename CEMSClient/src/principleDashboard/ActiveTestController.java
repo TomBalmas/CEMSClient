5fd9a503de01df1ec85e4708730fe7fa33a17607
vered <vered@192.168.1.15>
2021-05-13 22:43:49 +0300

    10:30

--- CEMSClient/src/principleDashboard/ActiveTestController.java
+++ CEMSClient/src/principleDashboard/ActiveTestController.java
@@ -4,16 +4,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXScrollPane;
-import com.jfoenix.controls.JFXTextArea;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 public class ActiveTestController implements Initializable {
@@ -27,17 +25,16 @@ public class ActiveTestController implements Initializable {
 	// -------------------------------------------------------
 
 	VBox vBoxScrollPane = new VBox();
-
+	
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
+			
+		vBoxScrollPane.setPadding(new Insets(0,0,0,210));
 		addTeacherRequest("vered","tom");
 		addTeacherRequest("vered","tom");
 		addTeacherRequest("vered","tom");
-		// for (int i = 0; i < 10; i++)
-		// vBoxScrollPane.getChildren().add(new JFXButton(i + ""));
-
-		// scrollPane.setContent(vBoxScrollPane);
+	
 
 	}
 
@@ -48,11 +45,10 @@ public class ActiveTestController implements Initializable {
 			Node request = loader.load();
 			TeacherRequestController controller = loader.getController();
 			controller.getTeacherName().setText(teacherName);
-			controller.getTextArea().setText(requestMsg);
-			
-			vBoxScrollPane.getChildren().add(request);
+			controller.getTextArea().setText(requestMsg);		
+			vBoxScrollPane.getChildren().add(request);	
 			scrollPane.setContent(vBoxScrollPane);
-
+		
 		} catch (IOException e) {
 
 			e.printStackTrace();
